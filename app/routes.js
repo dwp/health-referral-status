c6748e9354f7c60abfa8a839e6726cd895661046
Leah Smith <leah.smith3@engineering.digital.dwp.gov.uk>
2022-03-24 15:52:55 +0000

    added third prototype

--- app/routes.js
+++ app/routes.js
@@ -815,6 +815,32 @@ router.post('/next-action/v2/status-filter2', function (req, res) {
   }
 })
 
+router.post('/next-action/v3/status-filter2', function (req, res) {
+  
+  const status2 = req.session.data['status2']
+  req.session.data['status2']=''
+
+  if (status2 === 'list-1') {
+    res.redirect('/next-action/v3/list-1')
+  } else if (status2 === 'list-2') {
+      res.redirect('/next-action/v3/list-2')
+  } else if (status2 === 'list-3') {
+    res.redirect('/next-action/v3/list-3')
+  } else if (status2 === 'assigned') {
+    res.redirect('/next-action/v3/assigned-list')
+  } else if (status2 === 'in-review') {
+    res.redirect('/next-action/v3/review-list')
+  } else if (status2 === 'awaiting-evidence') {
+    res.redirect('/next-action/v3/evidence-list')
+  } else if (status2 === 'assessment-ready') {
+    res.redirect('/next-action/v3/assessment-list')
+  } else if (status2 === 'awaiting-decision') {
+    res.redirect('/next-action/v3/decision-list')
+  } else {
+    res.redirect('/next-action/v3/error')
+  }
+})
+
 // Collaboration
 
 router.post('/colab/v1/status-filter', function (req, res) {

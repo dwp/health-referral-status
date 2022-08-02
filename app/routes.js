40346126b5e11db6739c5ec9b7c1dcd26212fe5d
Leah Smith <leah.smith3@engineering.digital.dwp.gov.uk>
2022-08-02 09:02:11 +0100

    creation of filter prototype that reflects live

--- app/routes.js
+++ app/routes.js
@@ -963,6 +963,28 @@ router.post('/filter/v2/status-filter', function (req, res) {
   }
 })
 
+router.post('/filter/v3/status-filter', function (req, res) {
+  
+  const status2 = req.session.data['status']
+  req.session.data['status']=''
+
+  if (status2 === 'list-1') {
+    res.redirect('/filter/v3/list-1')
+  } else if (status2 === 'list-2') {
+      res.redirect('/filter/v3/list-2')
+  } else if (status2 === 'list-3') {
+    res.redirect('/filter/v3/list-3')
+  } else if (status2 === 'assigned') {
+    res.redirect('/filter/v3/list-4')
+  } else if (status2 === 'hcp-review') {
+    res.redirect('/filter/v3/list-5')
+  } else if (status2 === 'report-ready') {
+    res.redirect('/filter/v3/list-6')
+  } else {
+    res.redirect('/filter/v3/error')
+  }
+})
+
 // Collaboration
 
 router.post('/colab/v1/status-filter', function (req, res) {

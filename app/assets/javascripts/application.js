a787793313dc93307aeaa592232af8c12977aa3f
Leah Smith <leah.smith3@engineering.digital.dwp.gov.uk>
2022-09-02 11:17:27 +0100

    added MOJ library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,8 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+  window.MOJFrontend.initAll()
+})
